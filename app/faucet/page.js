3a4dd3a78ecbe21fbd8dc8ed829646011d544dd4
Raebeatrose <rasbinbot@gmail.com>
2024-11-10 15:51:44 +0530

    logic for requesting tokens

--- app/faucet/page.js
+++ app/faucet/page.js
@@ -5,12 +5,24 @@ import { Header } from "@/components/Header/Header";
 import CustomCursor from "@/components/CustomCursor";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import axios from "axios";
+import { Alert } from "@/components/ui/alert";
 
 export default function Faucet() {
+  const [address, setAddress] = useState('');
+  const [error, setError] = useState(null);
+
   const requestNzt = async () => {
-    // Make request to backend for requesting tokens 
-    console.log("Requesting 1 NZT");
-  }
+    try {
+      const response = await axios.post("https://evm-faucet-devnet.nexscan.io/api", { address });
+      console.log(response.data);
+      setError(null);
+    } catch (error) {
+      console.error("Error making the POST request:", error);
+      setError("Unable to request");
+    }
+  };
 
   return (
     <div className="relative flex min-h-screen flex-col">
@@ -23,15 +35,22 @@ export default function Faucet() {
             Enter your wallet address to receive 1 NZT for testing dApps on our TESTNET
           </p>
           <p className="text-gray-600 mb-4 text-sm">
-          (NOTE: Testnet token's dont hold any physical significance)
+            (NOTE: Testnet tokens don’t hold any physical significance)
           </p>
+          
           <Input
             className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-lg"
             placeholder="Wallet Address"
+            onChange={(e) => setAddress(e.target.value)}
           />
           <Button className="w-full" onClick={requestNzt}>
             Request 1 NZT
           </Button>
+          {error && (
+            <Alert className="mb-4 text-red-600">
+              {error}
+            </Alert>
+          )}
         </div>
       </div>
       <Footer />
